c439bda5e83f9fffdace5dca9a107350734255c7
51m0n2063r <simon.roger31@gmail.com>
2018-06-25 11:22:56 +0200

    add Slider

--- js/init.js
+++ js/init.js
@@ -1,7 +1,24 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.sidenav').sidenav();
+        $('.sidenav').sidenav();
 
-  }); // end of document ready
+    });
+    document.addEventListener('DOMContentLoaded', function () {
+        var elems = document.querySelectorAll('.datepicker');
+        var instances = M.Datepicker.init(elems, options);
+    });
+    document.addEventListener('DOMContentLoaded', function () {
+        var elems = document.querySelectorAll('.slider');
+        var instances = M.Slider.init(elems, options);
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
+$(document).ready(function () {
+    $('.datepicker').datepicker();
+});
+$(document).ready(function () {
+    $('.parallax').parallax();
+});
+ $(document).ready(function(){
+    $('.slider').slider();
+  });
